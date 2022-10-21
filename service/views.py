70ee7b83166dc48f9ce6409786f725e78135dc79
kasai <kasaiyuhei8@gmail.com>
2022-10-21 21:36:18 +0900

    チケットの完了フラグの編集をできるようにする

--- service/views.py
+++ service/views.py
@@ -4,17 +4,19 @@ from .forms import TicketCreateForm
 
 
 def index(request):
-    tickets = Ticket.objects.all()
-    return render(request, "index.html", {"tickets": tickets})
+    if request.method == "POST":
+        ticket = Ticket.objects.filter(title=list(request.POST.keys())[1]).first()
+        ticket.is_done = False if ticket.is_done else True
+        ticket.save()
+        return render(request, "index.html", {"tickets": Ticket.objects.all()})
+    return render(request, "index.html", {"tickets": Ticket.objects.all()})
 
 
 def create(request):
     form = TicketCreateForm()
-    print("form")
     if request.method == "POST":
         form = TicketCreateForm(request.POST)
         if form.is_valid():
-            print(form)
             form.save()
         return redirect("/")
     return render(request, "create.html", {"form": form})
